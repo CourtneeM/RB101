21c68288fe68f26353071769912c7e56358c0d4f
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-02-11 09:51:30 -0600

    Add rock paper scissors bonus feature 3

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -17,16 +17,6 @@ def win?(first, second)
     (first == 'spock' && second == 'scissors')
 end
 
-def display_results(player, computer)
-  if win?(player, computer)
-    prompt("You won!")
-  elsif win?(computer, player)
-    prompt("Computer won!")
-  else
-    prompt("It's a tie!")
-  end
-end
-
 def abbr_choice_to_choice(choice)
   VALID_CHOICES.select do |valid_choice|
     if valid_choice.start_with?('s')
@@ -37,28 +27,83 @@ def abbr_choice_to_choice(choice)
   end.join
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
-    choice = abbr_choice_to_choice(choice) if !VALID_CHOICES.include?(choice)
+def who_won?(player, computer)
+  if win?(player, computer)
+    'player'
+  elsif win?(computer, player)
+    'computer'
+  else
+    'tie'
+  end
+end
 
-    if VALID_CHOICES.include?(choice.downcase)
-      break
-    else
-      prompt("That's not a valid choice.")
+def increment_win(round_result, score)
+  if round_result == 'player'
+    score[:player] += 1
+  elsif round_result == 'computer'
+    score[:computer] += 1
+  end
+  score
+end
+
+def check_for_winner(score)
+  score[:player] == 5 || score[:computer] == 5
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+def display_grand_winner(score)
+  if score[:player] == 5
+    prompt("You're the grand winner!")
+  elsif score[:computer] == 5
+    prompt("The computer is the grand winner!")
+  end
+end
+
+def display_current_scores(score)
+  prompt("Player wins: #{score[:player]} | Computer wins: #{score[:computer]}")
+end
+
+loop do
+  score = { player: 0, computer: 0 }
+
+  loop do
+    choice = ''
+
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      choice = gets.chomp
+      choice = abbr_choice_to_choice(choice) if !VALID_CHOICES.include?(choice)
+
+      if VALID_CHOICES.include?(choice.downcase)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
     end
+
+    computer_choice = VALID_CHOICES.sample
+    score = increment_win(who_won?(choice, computer_choice), score)
+
+    prompt("You chose: #{choice}. Computer chose: #{computer_choice}")
+    display_results(choice, computer_choice)
+    display_current_scores(score)
+
+    break if check_for_winner(score)
   end
 
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose: #{choice}. Computer chose: #{computer_choice}")
-
-  display_results(choice, computer_choice)
-
+  display_grand_winner(score)
   prompt("Do you want to play again?")
   answer = gets.chomp
+
   break unless answer.downcase.start_with?('y')
 end
 
