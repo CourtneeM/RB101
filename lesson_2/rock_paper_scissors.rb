76b13f6d75a1944e7a933780c01572562cc25280
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-02-16 11:06:50 -0600

    Refactor main loop into separate methods

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -10,6 +10,38 @@ def win?(first, second)
     (first == 'scissors' && second == 'paper')
 end
 
+def player_choice
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
+
+    if VALID_CHOICES.include?(choice)
+      return choice
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+end
+
+def play_again?
+  prompt("Do you want to play again?")
+
+  loop do
+    prompt("Yes to play again, No to exit.")
+    answer = gets.chomp
+
+    case answer.downcase
+    when 'y', 'yes' then return true
+    when 'n', 'no' then return false
+    else prompt("That isn't a valid choice.")
+    end
+  end
+end
+
+def display_round_choices(choice, computer_choice)
+  prompt("You chose: #{choice}. Computer chose: #{computer_choice}.")
+end
+
 def display_results(player, computer)
   if win?(player, computer)
     prompt("You won!")
@@ -20,28 +52,26 @@ def display_results(player, computer)
   end
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
-
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
-  end
-
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose: #{choice}. Computer chose: #{computer_choice}.")
-
-  display_results(choice, computer_choice)
-
-  prompt("Do you want to play again?")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+def display_welcome_message
+  prompt("Welcome to Rock, Paper, Scissors, Lizard, Spock!")
+  prompt("Whoever wins 5 rounds will be the grand winner. Good luck!")
+  prompt("----------------------------------------------------------")
 end
 
-prompt("Thank you for playing. Goodbye!")
+def display_goodbye_message
+  prompt("Thank you for playing. Goodbye!")
+end
+
+display_welcome_message
+
+loop do
+  choice = player_choice
+  computer_choice = VALID_CHOICES.sample
+
+  display_round_choices(choice, computer_choice)
+  display_results(choice, computer_choice)
+
+  break unless play_again?
+end
+
+display_goodbye_message
