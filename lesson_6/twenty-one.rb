a43077d11ae4351ba6165b90519c170c0cc4d664
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-03-11 15:44:25 -0600

    Add initialize deck method and CARD_VALUES

--- lesson_6/twenty-one.rb
+++ lesson_6/twenty-one.rb
@@ -1,4 +1,6 @@
 # 1. Initialize deck
+#  - 13 cards per suit(4)
+#  - create the deck by cycling through the suits and card numbers/faces
 # 2. Deal cards to player and dealer
 # 3. Player turn: hit or stay
 #   - repeat until bust or "stay"
@@ -15,8 +17,23 @@
 
 # Data Structures
 # card_values: hash { '1' => 1, 'jack' => 10, 'ace': [1, 11] }
-# used_cards: nested array that contains values from the deck nested array when
-#             they are in the player or dealer hand [['D', '2'], ['H', 'J']]
 # deck: nested array [['H', 5], ['C', 1], ['S', 'Q']]
 # player cards: array (for aces, include [1, 11] and decide which one to use when needed)
 # dealer cards: array  ^^^
+
+CARD_VALUES = { '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5,
+                '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10,
+                'J' => 10, 'Q' => 10, 'K' => 10, 'A' => [1, 11] }
+
+def initialize_deck
+  suits = ['H', 'D', 'C', 'S']
+  numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+  suits.map do |suit|
+    numbers.map do |number|
+      [suit, number]
+    end
+  end
+end
+
+p initialize_deck
