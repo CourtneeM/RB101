02a7179021433251bcf2e640f2cc7f3d8e4b7dc9
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-03-11 20:44:22 -0600

    Add methods to calculate aces and hand total

--- lesson_6/twenty-one.rb
+++ lesson_6/twenty-one.rb
@@ -23,9 +23,9 @@
 # player cards: array (for aces, include [1, 11] and decide which one to use when needed)
 # dealer cards: array  ^^^
 
-CARD_VALUES = { '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5,
-                '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10,
-                'J' => 10, 'Q' => 10, 'K' => 10, 'A' => [1, 11] }
+def prompt(message)
+  puts(">> #{message}")
+end
 
 def initialize_deck
   suits = ['H', 'D', 'C', 'S']
@@ -54,7 +54,85 @@ def remove_dealt_cards_from_deck!(deck, hand)
   hand.each { |card| deck.delete(card) }
 end
 
-deck = initialize_deck
-player_hand, dealer_hand = deal_cards(deck)
-p player_hand
-p dealer_hand
+def hit(deck, hand)
+  hand.push(deck.sample)
+  remove_dealt_cards_from_deck!(deck, hand)
+end
+
+def busted?(hand)
+  hand_total(hand) > 21
+end
+
+def hand_total(hand)
+  total = 0
+  aces = 0
+  hand.map do |card|
+    aces += 1 if card[1] == 'A'
+    total += 10 if ['J', 'Q', 'K'].include?(card[1])
+    total += card[1].to_i
+  end
+
+  p aces_total = calculate_aces(total, aces)
+  p total += aces_total
+end
+
+def calculate_aces(total, aces)
+  high_aces = aces
+  low_aces = 0
+  aces_total = 0
+
+  loop do
+    break aces_total if high_aces < 0
+
+    aces_total = (high_aces * 11) + (low_aces * 1)
+    if total + aces_total > 21
+      high_aces -= 1
+      low_aces += 1
+    else
+      break aces_total
+    end
+  end
+end
+
+def declare_winner(player_hand, dealer_hand)
+  # if busted?(player_hand)
+  #   dealer wins
+  # end
+end
+
+def player_turn(deck, player_hand)
+  loop do
+    prompt("hit or stay?")
+    answer = gets.chomp
+    if answer == 'hit'
+      hit(deck, player_hand)
+    end
+
+    p player_hand
+
+    break if answer == 'stay' || busted?(player_hand)
+  end
+
+  busted?(player_hand) ? return : prompt("You chose to stay!")
+end
+
+def dealer_turn(deck, dealer_hand)
+
+end
+
+loop do
+  deck = initialize_deck
+  player_hand, dealer_hand = deal_cards(deck)
+
+  player_turn(deck, player_hand)
+  if busted?(player_hand)
+    declare_winner(player_hand, dealer_hand)
+  else
+    dealer_turn(deck, dealer_hand)
+  end
+  # declare_winner(player_hand, dealer_hand)
+
+  prompt("Do you want to play again? (yes or no)")
+  answer = gets.chomp.downcase
+  break unless answer == 'yes'
+end
