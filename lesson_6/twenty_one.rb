255033bd15b87db6bae9152be5e175dc2ce4dbeb
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-03-16 20:10:53 -0500

    Add methods to detect and declare winner. Correctly calculate royal cards

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -32,8 +32,8 @@ def display_welcome
 end
 
 def display_hands(player_hand, dealer_hand, totals, results = false)
-  player_values = get_values(player_hand)
-  dealer_values = get_values(dealer_hand)
+  player_values = values(player_hand)
+  dealer_values = values(dealer_hand)
 
   case results
   when true
@@ -60,6 +60,7 @@ def deal_cards(deck)
 end
 
 def player_turn(player_hand, dealer_hand, deck, totals)
+  totals['player'] = total(values(player_hand))
   loop do
     display_hands(player_hand, dealer_hand, totals)
 
@@ -72,14 +73,11 @@ def player_turn(player_hand, dealer_hand, deck, totals)
     end
   end
 
-  if busted?(totals, 'player')
-    play_again?
-  else
-    puts "You chose to stay!"
-  end
+  busted?(totals, 'player') ? play_again? : puts("You chose to stay!") # needs fix
 end
 
 def dealer_turn(dealer_hand, player_hand, deck, totals)
+  totals['dealer'] = total(values(dealer_hand))
   loop do
     break if totals['dealer'] >= DEALER_HIT_UNTIL || busted?(totals, 'dealer')
     hit(dealer_hand, deck, totals, 'dealer')
@@ -102,19 +100,46 @@ end
 
 def hit(hand, deck, totals, current_player)
   hand << deck.pop
-  totals[current_player] = get_total(get_values(hand))
+  totals[current_player] = total(values(hand))
 end
 
-def busted?(totals, current_player)
+def busted?(totals, current_player) # fix
   totals[current_player] > 21
 end
 
-def get_values(hand)
+def detect_winner(totals)
+  player_total, dealer_total = totals.values_at('player', 'dealer')
+  if player_total > dealer_total && !busted?(totals, 'player')
+    :player
+  elsif dealer_total > player_total && !busted?(totals, 'dealer')
+    :dealer
+  end
+end
+
+def display_winner(winner)
+  case winner
+  when :player then puts "You won!"
+  when :dealer then puts "Dealer won!"
+  end
+end
+
+def values(hand)
   hand.map { |card| card[1] }
 end
 
-def get_total(hand_values) # need to handle royals
-  hand_values.map(&:to_i).sum
+def total(hand_values)
+  total = hand_values.map do |value|
+    if ['J', 'Q', 'K'].include?(value)
+      10
+    elsif value == 'A'
+      11
+    else
+      value.to_i
+    end
+  end.sum
+
+  hand_values.count('A').times { total -= 10 if total > 21 }
+  total
 end
 
 def play_again?
@@ -135,14 +160,18 @@ loop do
 
   deck = initialize_deck
   player_hand, dealer_hand = deal_cards(deck)
-  totals = { 'player' => get_total(get_values(player_hand)),
-             'dealer' => get_total(get_values(dealer_hand)) }
+  totals = { 'player' => 0,
+             'dealer' => 0 }
 
   player_turn(player_hand, dealer_hand, deck, totals)
   dealer_turn(dealer_hand, player_hand, deck, totals)
   display_hands(player_hand, dealer_hand, totals, true)
+  display_winner(detect_winner(totals))
 
   break unless play_again?
 end
 
 prompt("Thanks for playing! Goodbye.")
+
+
+# what to do when busted?
